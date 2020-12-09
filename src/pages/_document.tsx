cd27fc1699a8f10afd7d67fd8ad13c7fc938dad8
Costa <victorcosta37@gmail.com>
2020-12-09 14:41:05 -0300

    chore: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
-
+          <link rel="shortcut icon" href="./static/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
